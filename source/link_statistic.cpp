50be0acd69d7d02915e72452998bedbf30fa0ba5
Berke DURAK <berke.durak@gmail.com>
2010-07-21 18:28:51 +0200

    Forgotten files.

---
+++ source/link_statistic.cpp
@@ -0,0 +1,118 @@
+// link_statistic.cpp
+//
+// Author: Berke Durak <berke.durak@gmail.com>
+
+#include "link_statistic.hpp"
+
+using namespace std;
+
+link_statistic::link_statistic(nat running_average_window_, nat maximum_window_) :
+  count(0),
+  total(0),
+  start(microsecond_timer::get()),
+  items(running_average_window_),
+  buffer_total(0),
+  max_circular(maximum_window_)
+{
+}
+
+void link_statistic::add(size_t size, microsecond_timer::microseconds t)
+{
+  if(items.full())
+  {
+    item i = items.back();
+    buffer_total -= i.size;
+  }
+  item i_new(size, t);
+  items.push_front(i_new);
+  count ++;
+  total += size;
+  buffer_total += size;
+
+  double bw = average_bandwidth();
+  if(max_circular.full())
+  {
+    max_set.erase(max_circular.back());
+  }
+  max_circular.push_front(bw);
+  max_set.insert(bw);
+}
+
+double link_statistic::max_bandwidth() const
+{
+  multiset<double>::iterator it = max_set.end();
+  if(it == max_set.begin()) return 0;
+  -- it;
+  return *it;
+}
+
+double link_statistic::instantaneous_bandwidth() const
+{
+  if(items.size() < 2) return 0;
+
+  boost::circular_buffer<item>::const_iterator it = items.begin();
+  const item& i1 = *it;
+  it ++;
+  const item& i2 = *it;
+
+  double dt = 1e-6 * (i1.when - i2.when);
+  size_t dB = i1.size;
+  if(dt > 0)
+  {
+    return dB/dt/1e3;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+double link_statistic::average_bandwidth() const
+{
+  if(items.size() < 2)
+  {
+    return 0;
+  }
+  else
+  {
+    size_t sub_total = buffer_total - items.front().size;
+    double dt = average_duration();
+    if(dt > 0)
+    {
+      return sub_total/dt/1e3;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+}
+
+double link_statistic::average_duration() const
+{
+  return items.empty() ? 0.0 : 1e-6 * (items.front().when - items.back().when);
+}
+
+ostream& operator<<(ostream& out, const link_statistic& self)
+{
+  if(self.count < 2)
+  {
+    out << "NA";
+  }
+  else
+  {
+    const double t_total = (microsecond_timer::get() - self.start) * 1e-6;
+    const double t_average = self.average_duration();
+
+    out << 
+      "total "             <<
+      self.count           << " packets, " <<
+      self.total/1e3       << " kB in " <<
+      t_total              << " s; " <<
+      " bw " <<
+        self.instantaneous_bandwidth() << " kB/s instantaneous, " <<
+        self.average_bandwidth()       << " kB/s average (over " << t_average << " s at " << self.items.size()/t_average    << " packet/s), " <<
+        self.max_bandwidth()           << " kB/s max (over " << self.max_circular.size() << " samples), ";
+  }
+  return out;
+}
---
+++ source/link_statistic.hpp
@@ -0,0 +1,66 @@
+// link_statistic.hpp
+//
+// Author: Berke Durak <berke.durak@gmail.com>
+
+#ifndef LINK_STATISTIC_HPP_20100521
+#define LINK_STATISTIC_HPP_20100521
+
+#include <iostream>
+
+#include <set>
+#include <boost/circular_buffer.hpp>
+
+#include "shorthands.hpp"
+#include "microsecond_timer.hpp"
+
+/// \brief Maintain aggregate link statistics.
+/// This class computes a running bandwidth average, used for reporting.
+class link_statistic
+{
+  nat    count;
+  size_t total;
+  microsecond_timer::microseconds start;
+
+  struct item
+  {
+    size_t size;
+    microsecond_timer::microseconds when;
+
+    explicit item(size_t size_, microsecond_timer::microseconds when_) : size(size_), when(when_) { }
+  };
+  boost::circular_buffer<item> items; // For running average bandwidth computation
+  size_t buffer_total;
+
+  boost::circular_buffer<double> max_circular;
+  std::multiset<double> max_set;
+
+public:
+  /// Construct a link statistic computing object
+  /// \param running_average_window Compute the average instantaneous bandwidth over the last this many samples
+  /// \param maximum_window         Compute the maximum instantaneous bandwidth over the last this many samples
+  link_statistic(nat running_average_window=10, nat maximum_window=100);
+
+  /// Notify that a packet has been transmitted.
+  /// \param size The size of the packet in bytes
+  /// \param t    The time at which the packet has been transmitted (defaults to now)
+  void add(size_t size, microsecond_timer::microseconds t=microsecond_timer::get());
+
+  /// Return the average bandwidth.
+  /// \returns Average bandwidth over the specificed number of samples, in kB/s
+  double average_bandwidth() const;
+
+  /// Return the duration over which the average has been computed.
+  double average_duration() const;
+
+  /// Return the instantaneous bandwidth.
+  /// \returns Instantaneous bandwidth over the specificed number of samples, in kB/s
+  double instantaneous_bandwidth() const;
+
+  /// Return the maximum bandwidth.
+  /// \returns Maximum instantaneous bandwidth over the specificed number of samples, in kB/s
+  double max_bandwidth() const;
+
+  friend std::ostream& operator<<(std::ostream& out, const link_statistic& self);
+};
+
+#endif
