e565828ad0559d231da755bbdf9a215ba4681bab
Berke DURAK <berke.durak@advantechamt.com>
2010-08-11 14:39:54 -0400

    Rename.

--- source/curx.c
+++ source/curx.c
@@ -1,48 +1,5 @@
-#include <inttypes.h>
-#include <stddef.h>
 #include <arpa/inet.h>
-
-#define CURX_MISS_CHECKER_LG2_WINDOW 5
-#define CURX_MISS_CHECKER_WINDOW (1 << CURX_MISS_CHECKER_LG2_WINDOW)
-#define CURX_MISS_CHECKER_WINDOW_MASK (CURX_MISS_CHECKER_WINDOW - 1)
-
-struct curx_miss_checker_result
-{
-   int is_duplicate;
-   int some_missing;
-   uint32_t first_missing, last_missing;
-};
-
-struct curx_miss_checker
-{
-   uint64_t duplicates, missing, original;
-   uint32_t seen[CURX_MISS_CHECKER_WINDOW];
-   unsigned int seen_start, seen_size;
-   struct curx_miss_checker_result result;
-};
-
-enum curx_status
-{
-   CURX_OK    = 0,
-   CURX_SHORT = 1,
-   CURX_BAD   = 2,
-   CURX_OOO   = 4,
-   CURX_DUP   = 8,
-   CURX_TRUNC = 16
-};
-
-struct curx_wprng
-{
-  uint32_t a, b, c, d;
-};
-
-struct curx_state
-{
-   struct curx_wprng rng;
-   uint64_t seq_min, seq_max, seq_last, out_of_order, count, decodable_count,
-            byte_count, bad_checksum, truncated, total_errors, total_erroneous;
-   struct curx_miss_checker mc;
-};
+#include "curx.h"
 
 void (*curx_output_missing_hook)(uint32_t, uint32_t, uint32_t) = NULL;
 
@@ -52,7 +9,7 @@ static inline uint32_t curx_wprng_rol32(uint32_t x, uint32_t y)
    return (x << y) | (x >> (32 - y));
 }
 
-void curx_wprng_init(struct curx_wprng *g, uint32_t seed)
+static void curx_wprng_init(struct curx_wprng *g, uint32_t seed)
 {
    g->a = 0xdeadbeef ^ seed;
    g->b = 0x0badcafe + seed;
@@ -60,7 +17,7 @@ void curx_wprng_init(struct curx_wprng *g, uint32_t seed)
    g->d = 0xdefaced1 ^ seed;
 }
 
-void curx_wprng_step(struct curx_wprng *g)
+static void curx_wprng_step(struct curx_wprng *g)
 {
    g->a = curx_wprng_rol32(g->a, g->d);
    g->b ^= 0x89abcdef;
@@ -76,7 +33,7 @@ void curx_wprng_step(struct curx_wprng *g)
    g->d ^= 0x54581414;
 }
 
-uint32_t curx_wprng_get(struct curx_wprng *g)
+static uint32_t curx_wprng_get(struct curx_wprng *g)
 {
    curx_wprng_step(g);
    return g->a;
@@ -97,15 +54,7 @@ void curx_init(struct curx_state *q)
    q->total_erroneous = 0;
 }
 
-struct curx_ph
-{
-   uint32_t sequence;
-   uint32_t timestamp;
-   uint16_t size;
-   uint16_t check;
-};
-
-void curx_ph_fix_endianness(struct curx_ph *h)
+static void curx_ph_fix_endianness(struct curx_ph *h)
 {
    h->sequence  = ntohl(h->sequence);
    h->timestamp = ntohl(h->timestamp);
@@ -113,17 +62,17 @@ void curx_ph_fix_endianness(struct curx_ph *h)
    h->check     = ntohs(h->check);
 }
 
-uint16_t curx_ph_get_checksum(struct curx_ph *h)
+static uint16_t curx_ph_get_checksum(struct curx_ph *h)
 {
    return ~(h->sequence ^ h->size ^ h->timestamp);
 }
 
-int curx_ph_checksum_valid(struct curx_ph *h)
+static int curx_ph_checksum_valid(struct curx_ph *h)
 {
    return curx_ph_get_checksum(h) == h->check;
 }
 
-void curx_miss_checker_init(struct curx_miss_checker *c)
+static void curx_miss_checker_init(struct curx_miss_checker *c)
 {
    c->duplicates = 0;
    c->missing    = 0;
@@ -132,13 +81,13 @@ void curx_miss_checker_init(struct curx_miss_checker *c)
    c->seen_size  = 0;
 }
 
-void curx_miss_checker_insert(struct curx_miss_checker *c, unsigned int seq)
+static void curx_miss_checker_insert(struct curx_miss_checker *c, unsigned int seq)
 {
    c->seen[(c->seen_start + c->seen_size) & CURX_MISS_CHECKER_WINDOW_MASK] = seq;
    c->seen_size ++;
 }
 
-void curx_miss_checker_remove_oldest(struct curx_miss_checker *c, struct curx_miss_checker_result *r)
+static void curx_miss_checker_remove_oldest(struct curx_miss_checker *c, struct curx_miss_checker_result *r)
 {
    if(c->seen_size >= 2)
    {
@@ -160,7 +109,7 @@ void curx_miss_checker_remove_oldest(struct curx_miss_checker *c, struct curx_mi
    }
 }
 
-void curx_miss_checker_add(struct curx_miss_checker *c, uint32_t seq)
+static void curx_miss_checker_add(struct curx_miss_checker *c, uint32_t seq)
 {
    unsigned int i;
    struct curx_miss_checker_result *r = &c->result;
